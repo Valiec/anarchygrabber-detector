b5708c7f2895db8dfa0d2cb6ea667009cf19ed97
Valiec <valiec2019@gmail.com>
2020-06-01 22:00:28 -0400

    Initial commit.

---
+++ detect.py
@@ -0,0 +1,74 @@
+import platform
+import os
+import sys
+
+goodstr = "module.exports = require('./core.asar');"
+
+badstrs = ["4n4rchy", "inject", "hook", "modDir"]
+
+def get_latest_version(dirs):
+	latest = ""
+	for dir in dirs:
+		if dir.startswith("0"):
+			if dir>latest:
+				latest = dir
+	return latest
+			
+
+def check_discord(path, name):
+	dirs = os.listdir(path)
+	latest = get_latest_version(dirs)
+	print("Version is "+latest)
+	with open(path+"/"+latest+"/modules/discord_desktop_core/index.js") as f:
+		content = f.read()
+		badcount = 0	
+		for badstr in badstrs:
+			if badstr in content:
+				badcount+=1
+		if badcount>0:
+			print("DANGER: Discord "+name+" is likely INFECTED with AnarchyGrabber, "+str(badcount)+" signatures found!")
+		elif content == goodstr:
+			print("SUCCESS: Discord "+name+" is OK, no traces of AnarchyGrabber found!")
+		else:
+			print("WARNING: Discord "+name+" index.js content unknown. If you do not have a modified client, this may be an AnarchyGrabber infection!")
+		
+
+osname = platform.system()
+
+discordbase = ""
+
+if osname == "Darwin":
+	homedir = os.path.expanduser("~")
+	discordbase = homedir+"/Library/Application Support"
+	print("OS is Mac OS X")
+
+elif osname == "Windows":
+	homedir = os.path.expanduser("~")
+	discordbase = homedir+"/AppData/Roaming"
+	print("OS is Windows")
+
+else:
+	print("This tool does not currently support "+osname+"!")
+	sys.exit(1)
+
+print("Checking for Discord Stable...")
+if os.path.exists(discordbase+"/discord"):
+	print("Discord found at "+discordbase+"/discord")
+	check_discord(discordbase+"/discord", "Stable")
+else:
+	print("Discord Stable not found.")
+
+print("\nChecking for Discord PTB...")
+if os.path.exists(discordbase+"/discordptb"):
+	print("Discord found at "+discordbase+"/discordptb")
+	check_discord(discordbase+"/discordptb", "PTB")
+else:
+	print("Discord PTB not found.")
+
+print("\nChecking for Discord Canary...")
+if os.path.exists(discordbase+"/discordcanary"):
+	print("Discord found at "+discordbase+"/discordcanary")
+	check_discord(discordbase+"/discordcanary", "Canary")
+else:
+	print("Discord Canary not found.")	
+
